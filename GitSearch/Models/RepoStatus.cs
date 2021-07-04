8fd26e1772f64965a8fae68aca3fb71f8400754f
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-07-03 20:34:34 -0400

    Expand RepoStatus class

--- GitSearch/Models/RepoStatus.cs
+++ GitSearch/Models/RepoStatus.cs
@@ -2,7 +2,7 @@
 
 namespace GitSearch.Models
 {
-	class RepoStatus
+	public class RepoStatus
 	{
 		private DirectoryInfo directory;
 
@@ -17,14 +17,66 @@ namespace GitSearch.Models
 			set { directory = new DirectoryInfo(value); }
 		}
 
-		public bool DetachedHead { get; set; }
-
 		public string Branch { get; set; }
+		
+		public bool DetachedHead
+		{ 
+			get { return "HEAD".Equals(Branch); }
+		}
 
-		public bool LocalChanges { get; set; }
+		public bool HasUntracked 
+		{ 
+			get { return hasUntracked; }
+			set { hasUntracked = value; } 
+		}
+		private bool hasUntracked;
 
-		public bool UnpushedChanges { get; set; }
+		public bool HasModified
+		{ 
+			get { return hasModified; }
+			set { hasModified = value; }
+		}
+		private bool hasModified;
 
-		public bool RemoteChanges { get; set; }
+		public bool HasStaged
+		{
+			get { return hasStaged; }
+			set { hasStaged = value; }
+		}
+		private bool hasStaged;
+
+		public bool HasUnmerged
+		{
+			get { return hasUnmerged; }
+			set { hasUnmerged = value; }
+		}
+		private bool hasUnmerged;
+
+		public int? LocalCommits
+		{
+			get { return localCommits; }
+			set { localCommits = value; }
+		}
+		private int? localCommits;
+
+		public int? RemoteCommits
+		{ 
+			get { return remoteCommits; }
+			set { remoteCommits = value; }
+		}
+		private int? remoteCommits;
+
+		public bool IsUpToDate
+		{ 
+			get 
+			{
+				return !hasUntracked
+					&& !hasModified
+					&& !hasUnmerged
+					&& !hasStaged
+					&& (localCommits?.Equals(0) ?? true)
+					&& (remoteCommits?.Equals(0) ?? true);
+			} 
+		}
 	}
 }
